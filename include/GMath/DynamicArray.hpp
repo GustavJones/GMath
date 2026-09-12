2aa49c88a785ea95d21586299306875c732c07ab
GustavJones <gustavjones3@gmail.com>
2026-09-12 16:58:50 +0200

    Implement a dynamic array

--- include/GMath/DynamicArray.hpp
+++ include/GMath/DynamicArray.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #include "GMath/Types.hpp"
 #include <initializer_list>
-#include <vector>
 #include <iostream>
+#include <stdexcept>
 
 namespace GMath {
 /*
@@ -10,84 +10,212 @@ namespace GMath {
  */
 template <typename T> class DynamicArray {
 private:
-  std::vector<T> m_values;
+	T *m_data = nullptr;
+	GMath::size_t m_reserve = 0;
+	GMath::size_t m_size = 0;
 
 public:
   DynamicArray() = default;
-  explicit DynamicArray(const GMath::size_t _n) : m_values(_n) {};
-  DynamicArray(const T &_value) : m_values() { m_values.push_back(_value); }
-  DynamicArray(const std::initializer_list<T> &_list) : m_values(_list.begin(), _list.end()) {};
-  DynamicArray(DynamicArray &&) = default;
-  DynamicArray(const DynamicArray &) = default;
-  DynamicArray &operator=(DynamicArray &&) = default;
-  DynamicArray &operator=(const DynamicArray &) = default;
-  ~DynamicArray() = default;
+
+  explicit DynamicArray(const GMath::size_t _n) { Resize(_n); };
+
+  DynamicArray(const T &_value) { PushBack(_value); }
+
+  DynamicArray(const std::initializer_list<T> &_list) {
+		for (const auto &item : _list) {
+			EmplaceBack(item);
+		}
+	};
+
+  DynamicArray(DynamicArray &&_arr) : m_data(_arr.m_data), m_size(_arr.m_size), m_reserve(_arr.m_reserve) {
+		_arr.m_data = nullptr;
+		_arr.m_size = 0;
+		_arr.m_reserve = 0;
+	}
+
+  DynamicArray(const DynamicArray &_arr) : m_reserve(_arr.m_reserve), m_size(_arr.m_size) {
+		m_data = new T[m_reserve];
+
+		for (GMath::size_t i = 0; i < m_size; i++) {
+			m_data[i] = _arr.m_data[i];
+		}
+	}
+
+  DynamicArray &operator=(DynamicArray &&_arr) {
+		if (&_arr == this) {
+			return *this;
+		}
+
+		delete[] m_data;
+
+		m_data = _arr.m_data;
+		m_size = _arr.m_size;
+		m_reserve = _arr.m_reserve;
+
+		_arr.m_data = nullptr;
+		_arr.m_size = 0;
+		_arr.m_reserve = 0;
+
+		return *this;
+	}
+
+  DynamicArray &operator=(const DynamicArray &_arr) {
+		if (&_arr == this) {
+			return *this;
+		}
+
+		delete[] m_data;
+
+		m_size = _arr.m_size;
+		m_reserve = _arr.m_reserve;
+		m_data = new T[m_reserve];
+
+		for (GMath::size_t i = 0; i < m_size; i++) {
+			m_data[i] = _arr.m_data[i];
+		}
+
+		return *this;
+	}
+
+  ~DynamicArray() { delete[] m_data; }
 
   /*
    * Get the amount of items in the array.
    */
   [[nodiscard]]
-  GMath::size_t Size() const { return m_values.size(); };
+  GMath::size_t Size() const { return m_size; };
 
 	/*
 	 * Get data memory address.
 	 */
-	T *Data() { return m_values.data(); }
+	T *Data() { return m_data; }
 
 	/*
 	 * Get data memory address.
 	 */
-	const T *Data() const { return m_values.data(); }
+	const T *Data() const { return m_data; }
 
   /*
    * Set the amount of items in the array.
    */
-  void Resize(const GMath::size_t _size) { m_values.resize(_size); };
+  void Resize(const GMath::size_t _size) {
+		if (m_reserve < _size) {
+			Reserve(2 * _size);
+		}
+
+		m_size = _size;
+	};
 
 	/*
 	 * Reserve an amount of space for items in the array.
 	 */
-	void Reserve(const GMath::size_t _size) { m_values.reserve(_size); };
+	void Reserve(const GMath::size_t _size) {
+		if (_size == 0) {
+			delete[] m_data;
+			m_data = nullptr;
+			m_reserve = 0;
+			m_size = 0;
+			return;
+		}
+
+		T* data = new T[_size]();
+
+		for (GMath::size_t i = 0; i < _size && i < m_size; i++) {
+			data[i] = std::move(m_data[i]);
+		}
+
+		delete[] m_data;
+		m_data = data;
+		m_reserve = _size;
+
+		if (m_size > _size) {
+			m_size = _size;
+		}
+
+	};
 
   /*
    * Reset the array to empty.
    */
-  void Clear() { m_values.clear(); };
+  void Clear() { m_size = 0; };
 
   /*
    * Copy an object to the back of the array.
    */
-  void PushBack(const T &_val) { m_values.push_back(_val); }
+  void PushBack(const T &_value) { 
+		Insert(m_size, _value);
+	}
 
   /*
    * Move an object to the back of the array.
    */
-  void PushBack(T &&_val) { m_values.push_back(std::move(_val)); }
+  void PushBack(T &&_value) { 
+		Insert(m_size, std::move(_value));
+	}
 
   /*
    * Create an object at the back of the array.
    */
-  template <typename... Args> T &EmplaceBack(Args &&..._args) {
-    return m_values.emplace_back(std::move(_args...));
+  template <typename... Args> 
+	T &EmplaceBack(Args &&..._args) {
+		Resize(m_size + 1);
+
+		m_data[m_size - 1] = std::move(T(_args...));
+
+		return m_data[m_size - 1];
   }
 
   /*
    * Inserts a copy of an object at an index and moves the previous value one up.
    */
   void Insert(const GMath::size_t _index, const T &_value) {
-    if (_index == Size()) {
-      PushBack(_value);
-    }
-    else {
-      m_values.insert(m_values.begin() + _index, _value);
-    }
+		if (_index < 0 || _index > m_size) {
+			throw std::runtime_error("Index out of bounds.");
+		}
+
+		Resize(m_size + 1);
+
+		for (GMath::size_t i = m_size - 1; i > _index; i--) {
+			m_data[i] = std::move(m_data[i - 1]);
+		}
+
+		m_data[_index] = _value;
+  }
+
+  /*
+   * Inserts a copy of an object at an index and moves the previous value one up.
+   */
+  void Insert(const GMath::size_t _index, T &&_value) {
+		if (_index < 0 || _index > m_size) {
+			throw std::runtime_error("Index out of bounds.");
+		}
+
+		Resize(m_size + 1);
+
+		for (GMath::size_t i = m_size - 1; i > _index; i--) {
+			m_data[i] = std::move(m_data[i - 1]);
+		}
+
+		m_data[_index] = std::move(_value);
   }
 
   /*
    * Erase elements from the array.
    */
   void Erase(const GMath::size_t _index, const GMath::size_t _count = 1) {
-    m_values.erase(m_values.begin() + _index, m_values.begin() + _index + _count);
+		if (_index < 0 || _index >= m_size) {
+			throw std::runtime_error("Index out of bounds.");
+		}
+
+		if (_count < 1) {
+			return;
+		}
+
+		for (GMath::size_t i = _index; i < m_size - _count; i++) {
+			m_data[i] = std::move(m_data[i + _count]);
+		}
+
+		m_size -= _count;
   }
 
   /*
@@ -95,7 +223,7 @@ public:
    */
   [[nodiscard]]
   const T &operator[](const GMath::size_t _n) const {
-    return m_values[_n];
+    return m_data[_n];
   };
 
   /*
@@ -103,7 +231,7 @@ public:
    */
   [[nodiscard]]
   T &operator[](const GMath::size_t _n) {
-    return m_values[_n];
+    return m_data[_n];
   };
 };
 } // namespace GMath
